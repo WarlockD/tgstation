7d2282ad7f2d0ad93e8e2551c8c471f4a696b10d
Paul Bruner <warlockd@gmail.com>
2020-11-28 01:40:50 -0600

    woo we are rotating

--- tgui/packages/tgui/interfaces/SlotMachine.js
+++ tgui/packages/tgui/interfaces/SlotMachine.js
@@ -14,10 +14,6 @@ const Fsym = (props, context) => {
   } = props;
 return <div className={classes([image.sheet,image.sprite])}></div>
 };*/
-/// ok doing this in pure javascript test 2
-
-
-
 
 const Fsym = (props, context) => {
   const {
@@ -28,8 +24,7 @@ const Fsym = (props, context) => {
       <div
       className={classes([
         image.sheet,image.sprite])}
-        width="32px"
-        height="32px"/>
+        width="32px" height="32px" />
   );
   };
 
@@ -58,26 +53,74 @@ const symbols = [
   { sheet: "slot_machine32x32", sprite: "M5", x: 0, y: 0, rotate: 0 },
   { sheet: "slot_machine32x32", sprite: "M6", x: 0, y: 0, rotate: 0 },
 ];
-
+const FPS = 30;
+const Q = 10;
 class Slots extends Component {
   constructor(props,context) {
     super(props,context);
     this.slot_ref = createRef();
+    this.slots = [];
+    this.total_height = symbols.length * 32;
+    this.display_height = 5 * 32;
+
+
+    let slots = [];
+    for(let i=0; i < symbols.length; i++) {
+      slots[i] = { sheet: symbols[i].sheet, sprite: symbols[i].sprite, index : i };
+    }
+    this.state = {
+      'pos': 64,
+      'slots': slots,
+    };
   };
   componentDidMount() {
     logger.log("Do we have animate? " + this.slot_ref.current + " animate=" + this.slot_ref.current.hasOwnProperty('animate'));
+  }
+  tick() {
+    if(!Array.isArray(this.state.slots)) {
+      logger.log("Not a array!");
+      clearTimeout(this.timer);
+      return;
+    }
+    const currentValue = this.state.value;
+    const targetValue = 96;
+    const pos = currentValue * Q + targetValue * (1 - Q); // move 8 pixels?  is this smooth?
+    if(pos >= 96) {
+      let slots = [];
+      for(let i = 1; i < this.state.slots.length-1; i++)
+        slots.push(this.state.slots[i]);
+      slots.push(this.state.slots[0])
 
+      this.setState({'pos' : 64, 'slots': slots });
+    } else {
+      this.setState({'pos' : pos});
+    }
+
+
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(()=> this.tick(), 1000 / FPS);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
   }
 
 render() {
   const {
     className
   } = this.props;
+  const slots = this.state.slots;
+
   return (
     <div class={classes(["digit-container", className])} ref={this.slot_ref}>
-        {symbols.map((el, idx) => {
+        {slots.map((el, idx) => {
           return (
-            <Fsym symbol={el} key={idx} index={idx} image={el} />
+            <div key={idx} class={classes([el.sheet,el.sprite])}
+              width="32px" height="32px"
+              style={{ transform: 'translateY(' + this.state.pos + ')'}}
+              />
           );
         })}
     </div>
@@ -113,7 +156,7 @@ export class SlotMachine extends Component {
             <Slots className="slot-4" />
           </div>
           <div class="digit">
-            <Slots className="slot-5" />
+            <Slots  />
           </div>
         </div>
         </Box>
