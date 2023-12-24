47be5c7288f9fb080b5e2a880910ca8935bcd516
WarlockD <warlockd@gmail.com>
2023-12-24 11:27:34 -0600

    ugh

--- tgui/packages/tgui/interfaces/Terminal.tsx
+++ tgui/packages/tgui/interfaces/Terminal.tsx
@@ -1,11 +1,9 @@
-import { useLocalState } from '../backend';
-import { NoticeBox } from '../components';
 import { Window } from '../layouts';
-import { createRef, RefObject } from 'inferno';
+import { createLogger } from '../logging';
+import { Section } from '../components';
+import { createRef, forwardRef } from 'inferno';
+import XTTERM from 'xterm';
 //  import { Dialog, UnsavedChangesDialog } from '../components/Dialog';
-
-import XXTerm from 'xterm';
-import { ITerminalOptions, ITerminalInitOnlyOptions } from 'xterm';
 // import 'xterm/css/xterm.css';
 // import { Component, createRef, RefObject } from 'inferno';
 
@@ -14,35 +12,37 @@ const test_props = {
   cols: 80,
   rows: 24,
 };
-interface IProps {
-  className?: string;
-  innerRef?: RefObject<HTMLElement>;
-  options?: ITerminalOptions & ITerminalInitOnlyOptions;
-}
 
-const CXTerm = (props: IProps) => {
-  const xtermRef = props.innerRef ?? createRef();
-  const [input, setInput] = useLocalState('input', '');
-  const [xterm, setXTerm] = useLocalState(
-    'xterm',
-    new XXTerm.Terminal(props.options)
-  );
-  console.log('Start');
-  if (!xterm.element && xtermRef.current) {
-    xterm.open(xtermRef.current);
-  }
-
-  return <div className={classes([props.className])} ref={xtermRef} />;
+import { useLocalState } from '../backend';
+type ITerminal = {
+  uppertext: string;
+  messages: { key: string }[];
+  tguitheme: string;
 };
+type XTermProps = {
+  // ui_static_data
+  terminal?: XTTERM.Terminal;
+};
+const logger = createLogger('TXTerm');
 
-export const Terminal = (props) => {
-  const [input, setInput] = useLocalState('input', '');
-  const [xterm, setTerminal] = useLocalState(
-    'xterm',
-    new XXTerm.Terminal(test_props)
+const XTermContainer = forwardRef((props, ref) => (
+  <div id="xtermdisplay" ref={ref} />
+));
+const options = {
+  cols: 80,
+  rows: 24,
+};
+export const Terminal = (props: ITerminal) => {
+  const xtermref = createRef();
+  const [terminal123, setTerminal123] = useLocalState(
+    'termina123',
+    new XTTERM.Terminal(options)
   );
-  const xtermRef = createRef();
 
+  logger.debug('Terminal updated');
+  terminal123.open(document.getElementById('terminal'));
+  // const xtermRef = createRef();
+  /*
   const onEchoTest = (data: string) => {
     const code = data.charCodeAt(0);
     // If the user hits empty and there is something typed echo it.
@@ -65,21 +65,15 @@ export const Terminal = (props) => {
       setInput(input + data);
     }
   };
-  const options = {
-    cols: 80,
-    rows: 24,
-  };
+  */
 
+  //       <XTerm options={options} />
   console.log('Start');
   return (
     <Window title="Terminal" width={640} height={640}>
       <Window.Content scrollable>
-        <NoticeBox textAlign="left">{'Notice?'}</NoticeBox>
-        <CXTerm
-          options={options}
-          innerRef={xtermRef}
-          // onData=
-        />
+        <Section fill />
+        <div id="terminal" />
       </Window.Content>
     </Window>
   );
